db3b9953b6559fd79738e5498ed405a771c168bf
Tanner <triggdev@gmail.com>
2017-04-05 04:05:28 -0500

    finished myls, for now

--- myls.c
+++ myls.c
@@ -26,11 +26,22 @@ int gettotalblocks(struct dirent** list, int n) {
   int total = 0;
   struct stat info;
   for (int i = 0; i < n && stat(list[i]->d_name, &info) == 0; i++)
-    total += info.st_blocks / 2;
+    total += info.st_blocks / 2; // why divide by 2? idk it works
   return total;
 }
 
-void print(const char* name) {
+int getmaxdigit_size(struct dirent** list, int n) {
+  int max = 1;
+  struct stat info;
+  for (int i = 0; i < n && stat(list[i]->d_name, &info) == 0; i++) {
+    int n = info.st_size, d;
+    for (d = 0; n != 0; d++) n /= 10;
+    if (d > max) max = d;
+  }
+  return max;
+}
+
+void print(const char* name, int byte_pad) {
   struct stat info;
   if (stat(name, &info) == 0 && l_option) {
     struct passwd* pw = getpwuid(info.st_uid);
@@ -53,15 +64,15 @@ void print(const char* name) {
     printf(" "); // format better mmk
     printf("%s", g->gr_name);
     printf(" "); // format better mmk
-    printf("%ld", info.st_size);
+    printf("%*ld", byte_pad, info.st_size);
     printf(" "); // format better mmk
     printf("%s", months[t->tm_mon]);
     printf(" "); // format better mmk
-    printf("%d", t->tm_mday);
+    printf("%2d", t->tm_mday);
     printf(" "); // format better mmk
-    printf("%d", t->tm_hour);
+    printf("%.2d", t->tm_hour);
     printf(":"); // format better mmk
-    printf("%d", t->tm_min);
+    printf("%.2d", t->tm_min);
     printf(" "); // format better mmk
     printf("%s", name);
     printf("\n");
@@ -100,7 +111,7 @@ int main(int argc, char** argv) {
   n = scandir(dir, &list, filter, alphasort);
   
   if (n == -1 && errno == ENOTDIR) {
-    print(dir);
+    print(dir, 0);
     if (!l_option) printf("\n");
     return 0;
   }
@@ -110,9 +121,13 @@ int main(int argc, char** argv) {
   if (dirhandle)
     fd = dirfd(dirhandle); // use this for...well I don't know
   
-  if (l_option) printf("total %d\n", gettotalblocks(list, n));
+  if (l_option) 
+    printf("total %d\n", gettotalblocks(list, n));
+  
+  int byte_pad = getmaxdigit_size(list, n);
+  
   for (int i = 0; i < n; i++)
-    print(list[i]->d_name);
+    print(list[i]->d_name, byte_pad);
   
   if (!l_option) printf("\n");
   
